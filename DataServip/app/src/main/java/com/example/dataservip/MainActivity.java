a6a5dd0a6ff59a9cbda4119edc85b222d5358b18
Thomas <115964520+Tomassoft@users.noreply.github.com>
2023-11-20 16:53:26 -0500

    Vista V.2.0

--- DataServip/app/src/main/java/com/example/dataservip/MainActivity.java
+++ DataServip/app/src/main/java/com/example/dataservip/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.dataservip;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TIEMPO_DE_ESPERA = 3000; // Tiempo en milisegundos (3 segundos)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio2);
+        setContentView(R.layout.incio);
+
+        // Crear un Handler para manejar el retraso
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Crear un Intent para iniciar ActividadB
+                Intent intent = new Intent(MainActivity.this, inicio2.class);
+                startActivity(intent);
+                finish(); // Finalizar ActividadA para que no se pueda volver atrás
+            }
+        }, TIEMPO_DE_ESPERA);
     }
 }
