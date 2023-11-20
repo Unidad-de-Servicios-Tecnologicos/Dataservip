61b4deace7a12680725a9ff24eedc939405d60fd
Thomas <115964520+Tomassoft@users.noreply.github.com>
2023-11-20 16:35:40 -0500

    Vista
    Inicio, inicio 2 y consulta

--- DataServip/app/src/main/java/com/example/dataservip/inicio2.java
+++ DataServip/app/src/main/java/com/example/dataservip/inicio2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class inicio2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_consulta);
+        setContentView(R.layout.activity_inicio2);
     }
 }
