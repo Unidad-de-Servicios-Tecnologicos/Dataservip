c6223ebc59191cda933270155e8339df6134c218
Thomas <115964520+Tomassoft@users.noreply.github.com>
2023-11-21 10:03:45 -0500

    Vista bien

--- DataServip/app/src/main/java/com/example/dataservip/MainActivity.java
+++ DataServip/app/src/main/java/com/example/dataservip/MainActivity.java
@@ -7,21 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int TIEMPO_DE_ESPERA = 3000; // Tiempo en milisegundos (3 segundos)
+    private static final int TIEMPO_DE_ESPERA = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.incio);
 
-        // Crear un Handler para manejar el retraso
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Crear un Intent para iniciar ActividadB
                 Intent intent = new Intent(MainActivity.this, inicio2.class);
                 startActivity(intent);
-                finish(); // Finalizar ActividadA para que no se pueda volver atrás
+                finish();
             }
         }, TIEMPO_DE_ESPERA);
     }
--- DataServip/app/src/main/java/com/example/dataservip/inicio2.java
+++ DataServip/app/src/main/java/com/example/dataservip/inicio2.java
@@ -1,10 +1,12 @@
 package com.example.dataservip;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class inicio2 extends AppCompatActivity {
 
@@ -12,7 +14,17 @@ public class inicio2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio2);
+
+
+        Button myButton = findViewById(R.id.button2);
+        myButton.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.morado));
+
+
     }
 
+    public void irAActividadDos(View view) {
+        Intent intent = new Intent(this, consulta.class);
+        startActivity(intent);
+    }
 
 }
