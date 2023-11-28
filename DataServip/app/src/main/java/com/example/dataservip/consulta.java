ffdd2794cc19dfb9ea491bd3bd20cf6663ffd908
Thomas <115964520+Tomassoft@users.noreply.github.com>
2023-11-28 09:20:15 -0500

    Vista Conductor

--- DataServip/app/src/main/java/com/example/dataservip/consulta.java
+++ DataServip/app/src/main/java/com/example/dataservip/consulta.java
@@ -1,14 +1,28 @@
 package com.example.dataservip;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
 public class consulta extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta);
+
+
     }
+
 }
